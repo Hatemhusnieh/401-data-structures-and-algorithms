29c50aab594ae74665438ebccc9f5c07446daa1e
hatem <hatemhtm7@gmail.com>
2021-08-18 00:58:31 +0300

    challenge 32

--- Hash-Tables/index.js
+++ Hash-Tables/index.js
@@ -82,11 +82,11 @@ class Hashmap {
     array.forEach((word) => {
       if (shouldSkip) return;
 
-      word = word.split('');
-      if (word[word.length - 1] === ',') {
+      if (word.includes(',')) {
+        word = word.split('');
         word.pop();
         word = word.join('');
-      } else word = word.join('');
+      }
 
       word = word.toLowerCase();
       const hash = this.code(word);
@@ -100,6 +100,39 @@ class Hashmap {
       } else this.storage[hash].prepend({ [word]: word });
     });
     return repeated;
+  }
+
+  treeIntersection(tree1, tree2) {
+    if (!tree1.root && !tree2.root && !tree1 && !tree2) return 'empty tree/s';
+    const arr = [];
+
+    const addNode = (val) => {
+      if (this.storage[val]) {
+        this.storage[val].prepend(val);
+      } else {
+        const linkList = new LinkedList();
+        linkList.prepend(val);
+        this.storage[val] = linkList;
+      }
+    };
+
+    const addTree = (node) => {
+      if (node.value) addNode(node.value);
+      if (node.left) addTree(node.left);
+      if (node.right) addTree(node.right);
+    };
+    addTree(tree1.root);
+
+    const checkIntersection = (node) => {
+      if (this.storage[node.value]) {
+        arr.push(node.value);
+      }
+      if (node.left) checkIntersection(node.left);
+      if (node.right) checkIntersection(node.right);
+    };
+    checkIntersection(tree2.root);
+
+    return arr;
   }
 }
 
