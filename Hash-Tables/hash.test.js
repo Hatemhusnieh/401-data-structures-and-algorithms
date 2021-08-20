09d8b8fe62b38fd14e797532b277d66aaa70f807
hatem <hatemhtm7@gmail.com>
2021-08-20 11:45:21 +0300

    challenge 33

--- Hash-Tables/hash.test.js
+++ Hash-Tables/hash.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const Hashmap = require('./index');
+const { Hashmap, leftJoin } = require('./index');
 const { Node, Tree } = require('../Trees/tree');
 
 describe('Hash Map', () => {
@@ -23,10 +23,13 @@ describe('Hash Map', () => {
   test('should get the value of a key if exist', () => {
     const hashMap = new Hashmap(5000);
     hashMap.add('one', 'test1');
+    hashMap.add('eno', 'sss');
     hashMap.add('two', 'test2');
     hashMap.add('three', 'test3');
     expect(hashMap.get('one')).toBe('test1');
+    expect(hashMap.get('eno')).toBe('sss');
     expect(hashMap.get('two')).toBe('test2');
+    expect(hashMap.get('three')).toBe('test3');
     try {
       hashMap.get('four');
     } catch (error) {
@@ -52,6 +55,11 @@ describe('Hash Map', () => {
     expect(hashMap.hash('one')).toBe(hashMap.code('one'));
     expect(hashMap.hash('two')).toBe(hashMap.code('two'));
     expect(hashMap.hash('three')).toBe(hashMap.code('three'));
+    try {
+      hashMap.hash('six');
+    } catch (error) {
+      expect(error.message).toBe('key does not exist');
+    }
   });
 });
 
@@ -59,7 +67,8 @@ describe('Hash Map Repeated Words', () => {
   const case1 = 'Once upon a time, there was a brave princess who...';
   const case2 =
     'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
-  const case3 = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
+  const case3 =
+    'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
   test(' Case 1', () => {
     const hashMap = new Hashmap(5000);
     expect(hashMap.repeatedWord(case1)).toBe('a');
@@ -125,4 +134,22 @@ describe('Binary Tree Insertion', () => {
   expect(arr[2]).toBe(expected[2]);
   expect(arr[3]).toBe(expected[3]);
   expect(arr[4]).toBe(expected[4]);
+});
+
+describe(' LEFT JOIN', () => {
+  const map1 = new Hashmap(1000);
+  map1.add('a', 'aa');
+  map1.add('b', 'bb');
+  map1.add('c', 'cc');
+  const map2 = new Hashmap(1000);
+  map2.add('a', 'ao');
+  map2.add('c', 'co');
+  map2.add('d', 'dd');
+  test('should return a left join between two hash maps', () => {
+    const arr = leftJoin(map1, map2);
+    console.log(arr[0].toString());
+    expect(arr[0].toString()).toBe('a,aa,ao');
+    expect(arr[1].toString()).toBe('b,bb,null');
+    expect(arr[2].toString()).toBe('c,cc,co');
+  });
 });
